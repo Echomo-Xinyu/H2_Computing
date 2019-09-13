baa8b39a26f5021812a976025b7bf3215894f8bc
SEAN0905 <seanxinyu2@gmail.com>
2019-09-13 14:36:08 +0800

    completion of bs and k2k

--- revision/Promo/bs_k2k.py
+++ revision/Promo/bs_k2k.py
@@ -12,8 +12,52 @@ def bs(L, target):
     else: # L[mid] < target
         return bs(L[mid:], target)
 
+def bsI(L, target):
+    length = len(L)
+    l, r = 0, length-1
+    # note the equal sign here
+    while l <= r:
+        # update mid value accordingly
+        mid = (l + r) // 2
+        if L[mid] == target:
+            return True
+        elif L[mid] < target:
+            l = mid+1
+        else:
+            r = mid-1
+    return False
+
 A = [1, 2, 3, 4, 5, 6, 8, 9, 10]
-if bs(A, 8):
-    print("hey")
-if bs(A, 10):
-    print("hey")
+counter = 0
+for i in range(len(A)):
+    if bs(A, A[i]):
+        counter += 1
+if counter == len(A):
+    print("nice")
+    
+counter = 0
+for i in range(len(A)):
+    if bsI(A, A[i]):
+        counter += 1
+if counter == len(A):
+    print("nice")
+
+# take capital letter
+def d2k(num, k):
+    result = ""
+    mapping ="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    while num > 0:
+        digit = num % k
+        result = mapping[digit] + result
+        num = num // k
+    return result
+print(d2k(10, 2))
+
+def k2d(string, k):
+    mapping = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = 0
+    for i in range(len(string)):
+        result += mapping.index(string[i]) * k ** (len(string) - i - 1)
+    return result
+
+print(k2d("1010", 2))
